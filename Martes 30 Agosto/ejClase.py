0f0b6c52c82c03af537d06b30ce92f812d7fb70b
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2021-08-31 14:45:28 -0500

    Ejercio de clase

---
+++ Martes 30 Agosto/ejClase.py
@@ -0,0 +1,8 @@
+def detenteAntes(num):
+    "Se detiene si llega a un numero mayor a num, suma de 4 en 4"
+    inicio = 1
+    while(inicio < num):
+        anterior = inicio
+        inicio += 4
+    return anterior
+        
